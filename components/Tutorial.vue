5f9e6efb0a5bcc598b64dba486815359ab310b3c
Tom Dale <thomas.dale2@gmail.com>
2022-01-15 14:45:29 -0800

    initial commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+    <div class="absolute inset-0">
+      <div class="bg-white h-1/3 sm:h-2/3" />
+    </div>
+    <div class="relative max-w-7xl mx-auto">
+      <div class="text-center">
+        <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+          Hello World
+        </h2>
+        <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+           This is my demo, using Cosmic as a headless CMS and Nuxt.js as a frontend. Apollo graphQl is used to connect the API from Cosmic to Nuxt.  It is deployed using Vercel. 
+        </p>
+      </div>
+      <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+        <div v-for="post in data" :key="post.id" class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+          <div class="flex-shrink-0">
+            <nuxt-img
+              class="h-48 w-full object-cover"
+              :src="post.metadata.heroimage.imgix_url"
+              alt=""
+              provider="imgix"
+              sizes="xl:413px, lg:50vw, md:448px, sm:900px, xs:90vw)"
+              fit="crop"
+              :modifiers="{ auto: 'compress', crop: 'faces,edges', ar: '2.3:1'}"
+            />
+          </div>
+          <div class="flex-1 bg-white p-6 flex flex-col justify-between">
+            <div class="flex-1">
+                <p class="text-xl font-semibold text-gray-900">
+                  {{post.title}}
+                </p>
+                <p class="mt-3 text-base text-gray-500" v-html="post.content">
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data']
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial v-bind:data="getObjects.objects" />
+</template>
+
+<script>
+import getObjects from '../apollo/queries/posts.gql'
+export default {
+  apollo: {
+    getObjects: {
+      prefetch: true,
+      query: getObjects
+    }
+  }
+}
+</script>
