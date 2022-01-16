14dd0323cfb43171eeb0ca7334d30c7c69438d5d
Tom Dale <thomas.dale2@gmail.com>
2022-01-15 22:06:25 -0800

    fix css

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,6 @@
 <template>
-<div class="relative pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+<div class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+    <div class="relative max-w-7xl mx-auto">
     <div class="text-center">
         <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
           Cosmic to Nuxt Tutorial Demo
@@ -7,6 +8,7 @@
         <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
           This is a demo from Let's Go Headless,  an informational and tutorial website about Headless Web Architecture. This demo is using Cosmic as a headless CMS and Nuxt.js as a frontend. Apollo graphQl is used to connect the API from Cosmic to Nuxt.  It is deployed using Vercel. 
         </p>
+    </div>
     </div>
 </div>
 </template>
